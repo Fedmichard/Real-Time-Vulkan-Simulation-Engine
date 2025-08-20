07b780605db7fceb3571989e06540a6880c267b0
Fedmichard <francoisfedmichard@gmail.com>
2025-08-20 12:10:01 -0400

    stuff

--- build/libraries/SDL/include/SDL2/SDL_revision.h
+++ build/libraries/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.28.4-g31a4982 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.28.4-g5ccae87 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.28.4-g31a4982"
+#define SDL_REVISION "SDL-2.28.4-g5ccae87"
 #endif
